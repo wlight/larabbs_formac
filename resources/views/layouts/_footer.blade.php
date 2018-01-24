9dc5b52f9e330550844da70a31587c34923cce84
wlight <wormside@gmail.com>
2018-01-24 15:47:37 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/2187879951/profile?topnav=1&wvr=6" target="_blank">枫林残梦</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
