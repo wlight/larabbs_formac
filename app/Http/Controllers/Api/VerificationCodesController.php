6b02d7b9b72194dfcd2de59f1a38b88be4738668
wlight <wormside@gmail.com>
2018-05-20 02:46:43 +0000

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -11,7 +11,18 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
+
+        if (!$captchaData){
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)){
+            // 验证码错误就清空缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+        $phone = $captchaData['phone'];
 
         if (!app()->environment('production')){
             $code = '1234';
@@ -36,6 +47,9 @@ class VerificationCodesController extends Controller
 
         // 缓存验证码10分钟后过期
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
